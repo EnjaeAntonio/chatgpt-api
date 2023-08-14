693e6dd1f16f8ae0d4fade49f83940db722a758d
Enjae Antonio <enjaeantonio@outlook.com>
2023-08-14 14:44:26 -0500

    readme updates

--- script.js
+++ script.js
@@ -7,15 +7,14 @@ const openai = new OpenAIApi(new Configuration({ apiKey: process.env.API_KEY }))
 const userInterface = readline.createInterface({ input: process.stdin, output: process.stdout });
 userInterface.prompt();
 userInterface.on('line', async (input) => {
-    try{
-	const res = await openai.createChatCompletion({
-		model: 'gpt-3.5-turbo',
-		messages: [{ role: 'user', content: input }],
-	});
-	console.log(res.data.choices[0].message.content);
-    } catch(error){
-        console.log(error.message)
-    }
-
+	try {
+		const res = await openai.createChatCompletion({
+			model: 'gpt-3.5-turbo',
+			messages: [{ role: 'user', content: input }],
+		});
+		console.log(res.data.choices[0].message.content);
+	} catch (error) {
+		console.log(error.message);
+	}
 	userInterface.prompt();
 });
